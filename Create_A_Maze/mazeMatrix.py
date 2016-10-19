47090399c88fd940065402737429994a8deb2a34
Laurel Ruhlen <lruhlen@spins.com>
2016-10-19 17:45:36 -0500

    Complete cell connection methods

--- Create_A_Maze/mazeMatrix.py
+++ Create_A_Maze/mazeMatrix.py
@@ -18,10 +18,10 @@ class MazeMatrix(object):
                                 self.n_cols * self.cols_per_cell))
         self.room_list = list(itertools.product(range(self.n_cols),
                                                 range(self.n_rows)))
-                                    ## THESE INDICES AREN'T RIGHT.
-                                    ## COME BACK TO THIS.
-        self.relative_directions = {"North": (0,1), "South": (0,-1),
-                                    "East": (-1,0), "West":(1,0)}
+
+        self.relative_directions = {"West": (-1,0), "East": (1,0),
+                                    "South": (0,1), "North":(0,-1)}
+
         self.direction_pairings = {"North": "South",
                                    "South": "North",
                                    "East": "West",
@@ -39,6 +39,8 @@ class MazeMatrix(object):
 
 
     def get_neighbor_cell(self, (current_col, current_row), direction="North"):
+        print "Getting neighbor to the ", direction
+
         rel_dir = self.relative_directions[direction]
         (neighbor_col, neighbor_row) = (current_col + rel_dir[0],
                                         current_row + rel_dir[1])
@@ -46,6 +48,8 @@ class MazeMatrix(object):
         or (not (0 <= neighbor_row < self.n_rows))):
             return None
 
+        print "Neighbor row: ", neighbor_row
+        print "Neighbor column: ", neighbor_col
         return self.get_cell((neighbor_col, neighbor_row))
 
 
@@ -55,16 +59,28 @@ class MazeMatrix(object):
         return None
 
 
-    def connect_cells(self, (current_col, current_row), direction="North"):
+    def make_door(self, (current_col, current_row), direction="North"):
         current_cell = self.get_cell((current_col, current_row))
-        neighbor_cell = self.get_neighbor_cell((current_col, current_row),
-                                                direction=direction)
-        cur_cell_rel_dir = self.relative_directions[direction]
-        current_cell[cur_cell_rel_dir] = self.connect_flag
+        rel_dir = self.relative_directions[direction]
+        print direction, "is ", rel_dir[0], "in the x-direction, and", rel_dir[1], "in the y-direction"
+        door_coords = [x+1 for x in rel_dir]
+        print "door_coords = ", door_coords
+        current_cell[door_coords[1], door_coords[0]] = 2
 
-        nei_cnxn_dir = self.direction_pairings[direction]
-        nei_cell_rel_dir = self.relative_directions[nei_cnxn_dir]
-        neighbor_cell[nei_cell_rel_dir] = self.connect_flag
+    def connect_neighbors(self, (current_col, current_row), direction="North"):
+        try:
+            # Connect neighbor to current cell
+            nei_dir = self.relative_directions[direction]
+            nei_cnxn_dir = self.direction_pairings[direction]
+            nei_col = current_col + nei_dir[0]
+            nei_row = current_row + nei_dir[1]
+            self.make_door( (nei_col, nei_row), direction=nei_cnxn_dir)
+
+            # Connect current cell to its neighbor
+            self.make_door( (current_col, current_row), direction=direction)
+
+        except:
+            print "Can't connect this room in that direction."
 
         return None
 
@@ -73,3 +89,20 @@ class MazeMatrix(object):
         for row in self.matrix:
             print row
         return None
+
+
+def tests():
+    testMaze = MazeMatrix(2,2)
+    testMaze.print_maze()
+    print "\n\n"
+    testMaze.connect_neighbors((0,0), direction="South")
+    testMaze.print_maze()
+
+    print "\n\n"
+    testMaze.connect_neighbors((1,1), direction="West")
+    testMaze.print_maze()
+
+    print "\n\n"
+    testMaze.connect_neighbors((0,0), direction="North")
+    testMaze.print_maze()
+
